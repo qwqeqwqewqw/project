95c9b32198fc88eb06922a382e8ec27afedbefd9
Ojasvin Borawke <94136752+Pheonix747hs@users.noreply.github.com>
2025-07-04 22:21:25 +0530

    Integrate Tabler Icons Library

--- src/lib/RoomsGrid.svelte
+++ src/lib/RoomsGrid.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { IconCheck } from '@tabler/icons-svelte';
+  
   let { rooms } = $props();
 </script>
 
@@ -74,17 +76,7 @@
               <div class="grid sm:grid-cols-2 gap-2">
                 {#each room.features as feature}
                   <div class="flex items-center space-x-2">
-                    <svg
-                      class="w-5 h-5 text-forestGreen flex-shrink-0"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
+                    <IconCheck class="w-5 h-5 text-forestGreen flex-shrink-0" />
                     <span class="text-midnight">{feature}</span>
                   </div>
                 {/each}
