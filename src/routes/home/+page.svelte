15c1e861ab97225a363ecd0b2e0014bd4c6c7be6
Ojasvin Borawke <94136752+Pheonix747hs@users.noreply.github.com>
2025-07-06 20:27:01 +0530

    Updated +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,7 +2,7 @@
   import Header from "$lib/Header.svelte";
   import Hero from "$lib/Hero.svelte";
   import LocalTalents from "$lib/LocalTalents.svelte";
-  import Features from "$lib/Features.svelte";
+
   import Footer from "$lib/Footer.svelte";
   import Carousel from "$lib/Carousel.svelte";
   import MasonryCollage from "$lib/MasonryCollage.svelte";
