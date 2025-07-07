e66f5fee814ef3536f7970fb6cca704da46baf77
Ojasvin Borawke <94136752+Pheonix747hs@users.noreply.github.com>
2025-07-07 13:49:42 +0530

    Updated Hero.svelte

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -10,7 +10,7 @@
     <!-- Left: Content and Booking Form -->
     <div class="flex-1 flex flex-col justify-center px-8 xl:px-16 py-24">
       <div class="space-y-6 mb-12 max-w-4xl xl:pl-0">
-        <div class="font-cursive1 text-blue-800 text-9xl xl:text-6xl">
+        <div class="font-cursive1 text-blue-800  xl:text-6xl">
           Cherilyn Monta Resort
         </div>
         <h1
